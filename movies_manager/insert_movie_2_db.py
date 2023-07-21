fb3af3bf58fd130691242a61e4101410c928b3bb
MMDBadCoder <gitmmdgit@gmail.com>
2023-07-21 12:07:57 +0330

    insert method

--- movies_manager/insert_movie_2_db.py
+++ movies_manager/insert_movie_2_db.py
@@ -8,22 +8,35 @@ movies_index_range = range(1, 11)
 
 def insert():
     with open('all_movies.csv', 'r') as movies_file:
-        movies_file.readline()
-        while True:
-            if int(movies_file.readline().split('\t')[0]) in movies_index_range:
-                break
-        while True:
-            line = movies_file.readline().strip()
+
+        def try_to_create_model_object(line):
             parts = line.split('\t')
             index = int(parts[0])
-            if index not in movies_index_range:
-                break
             genre = parts[1]
             title1 = parts[2]
             title2 = parts[3]
-            votes_count = int()
+            votes_count = int(parts[4])
             rating = float(parts[5])
 
-            if not Movie.objects.filter(id=index):
-                raise Exception("Id of this movie is occupied by another movie: {}".format(line))
-            Movie.objects.create(id=index, title1=title1, title2=title2, votes_count=votes_count, rating=rating)
+            if Movie.objects.filter(id=index).exists():
+                print("Id of this movie is occupied by another movie: {}".format(line))
+                return
+            Movie.objects.create(id=index, title1=title1, title2=title2, genre=genre,
+                                 votes_count=votes_count, imdb_rating=rating)
+
+        def get_index_of_line(line):
+            return int(line.split('\t')[0])
+
+        movies_file.readline()
+        while True:
+            line = movies_file.readline()
+            index = get_index_of_line(line)
+            if index not in movies_index_range:
+                continue
+            try_to_create_model_object(line)
+            while True:
+                line = movies_file.readline()
+                index = get_index_of_line(line)
+                if index not in movies_index_range:
+                    return
+                try_to_create_model_object(line)
