24a9a47ffdc11d606de7494aa51acb3830528a8a
MMDBadCoder <gitmmdgit@gmail.com>
2023-07-21 11:56:49 +0330

    add genre to model

--- movies_manager/models.py
+++ movies_manager/models.py
@@ -6,12 +6,12 @@ from django.db import models
 class Movie(models.Model):
     title1 = models.CharField(max_length=100, blank=False, null=False)
     title2 = models.CharField(max_length=100, blank=False, null=False)
+    genre = models.CharField(max_length=100, blank=True, null=False, default='')
     votes_count = models.IntegerField(blank=False, null=False)
     imdb_rating = models.FloatField(blank=False, null=False)
     subtitle_download_url = models.CharField(max_length=1_000, blank=True, null=False, default='')
     video_stream_url = models.CharField(max_length=1_000, blank=True, null=False, default='')
     hidden_to_users = models.BooleanField(default=True, blank=False, null=False)
-
 
     def download_subtitle(self):
         pass
