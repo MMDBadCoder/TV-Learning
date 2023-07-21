401a2e2e1cdc1cbe56141cee66b98551a6e8369d
MMDBadCoder <gitmmdgit@gmail.com>
2023-07-21 11:48:06 +0330

    change rating to imdb rating

--- movies_manager/models.py
+++ movies_manager/models.py
@@ -7,7 +7,7 @@ class Movie(models.Model):
     title1 = models.CharField(max_length=100, blank=False, null=False)
     title2 = models.CharField(max_length=100, blank=False, null=False)
     votes_count = models.IntegerField(blank=False, null=False)
-    rating = models.FloatField(blank=False, null=False)
+    imdb_rating = models.FloatField(blank=False, null=False)
     subtitle_download_url = models.CharField(max_length=1_000, blank=True, null=False, default='')
     video_stream_url = models.CharField(max_length=1_000, blank=True, null=False, default='')
     hidden_to_users = models.BooleanField(default=True, blank=False, null=False)
